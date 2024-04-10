28e8110dae94488d76e52fc6e640f1f972f9ca36
ADABetti <alice.bet@hotmail.com>
2024-04-10 13:54:12 -0300

    Upload dos exercícios da lista 8

---
+++ programacao-procedural/Listas/Lista8/exercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+struct Pizza
+{
+  public int id;
+  public string flavor;
+  public double price;
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Pizza pizza1;
+    pizza1.id = int.Parse(Console.ReadLine());
+    pizza1.flavor = Console.ReadLine();
+    pizza1.price = double.Parse(Console.ReadLine());
+
+    Console.WriteLine(pizza1.id);
+    Console.WriteLine(pizza1.flavor);
+    Console.WriteLine(pizza1.price);
+  }
+}
---
+++ programacao-procedural/Listas/Lista8/exercicio2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+// criar struct:
+struct Product
+{
+  public int id;
+  public string name;
+  public double price;
+  public bool available;
+}
+
+class Program
+{
+
+  // Criar vetor:  
+  static void Main(string[] args)
+  {
+    Product[] products = new Product[4];
+
+    FillArray(products);
+    PrintIfAvailable(products);
+  }
+
+  // Preencher vetor:
+  static void FillArray(Product[] products)
+  {
+    for (int i = 0; i < products.Length; i++)
+    {
+      products[i].id = int.Parse(Console.ReadLine());
+      products[i].name = Console.ReadLine();
+      products[i].price = double.Parse(Console.ReadLine());
+      products[i].available = bool.Parse(Console.ReadLine());
+    }
+  }
+
+  // Imprimir produtos disponíveis:
+  static void PrintIfAvailable(Product[] products)
+  {
+    for (int i = 0; i < products.Length; i++)
+    {
+      if (products[i].available)
+      {
+        Console.WriteLine(products[i].name);
+        Console.WriteLine(products[i].price);
+      }
+    }
+  }
+}
